a72a243bcffc78e0248597cdabf5e146d3b11b3d
DECKDATA <eduardo_3185@hotmail.com>
2019-01-21 13:01:38 -0600

    Initial Commit

---
+++ IAmRich/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by CARLOS EDUARDO GALVAN CRUZ on 1/18/19.
+//  Copyright © 2019 CARLOS EDUARDO GALVAN CRUZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hola...")
+    }
+
+
+}
+
